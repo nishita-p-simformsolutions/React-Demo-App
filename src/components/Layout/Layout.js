004715feea310442223cf64b3a800374f5d5e1bb
nishita-p-simformsolutions <nishita.p@simformsolutions.com>
2021-02-26 14:06:56 +0530

    initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Layout = () => (
+    <div style={{paddingLeft: '10px'}}>
+        <h1>All Users</h1>
+        <p>Lorem ipsum dolor sir amet, consectetur esit, sed do elusomd tempor incicdicnt ut labore et dore magna aliqua. Ut eniunm minimum venim, quits nostrud</p>
+    </div>
+);
+
+export default Layout;
